a0978d37c9b0810f31537d2a2e911790ed38a47b
KevinCranmer <cranmerkevin39@gmail.com>
2025-02-01 12:47:35 -0600

    mgplay executable from commandblock now

--- src/main/java/com/chai/miniGolf/commands/PlayCommand.java
+++ src/main/java/com/chai/miniGolf/commands/PlayCommand.java
@@ -4,6 +4,8 @@ import com.chai.miniGolf.events.CoursePlayRequestedEvent;
 import com.chai.miniGolf.models.Course;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -23,32 +25,12 @@ import static com.chai.miniGolf.Main.getPlugin;
 public class PlayCommand implements CommandExecutor, TabCompleter
 {
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-    {
-        if (!sender.hasPermission("mgop"))
-        {
-            sender.sendMessage(ChatColor.WHITE + "[MiniGolf]" + ChatColor.RED + " You do not have permission to run this command.");
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (hasIssues(sender, args)) {
             return true;
         }
-        if (args.length < 1) {
-            sender.sendMessage(ChatColor.WHITE + "[MiniGolf]" + ChatColor.RED + " You need to provide a course name. Like: \"/mgplay Rolling Greens\".");
-            return true;
-        }
-        Optional<Player> maybePlayer = Bukkit.getOnlinePlayers().stream().filter(p -> p.getName().equals(args[args.length-1])).map(p -> (Player) p).findFirst();
-        int courseNameEndIndex;
-        Player golfer;
-        if (maybePlayer.isEmpty()) {
-            //Assume the player making the command is the player that will play the course
-            courseNameEndIndex = args.length;
-            golfer = (Player) sender;
-        } else {
-            if (args.length < 2) {
-                sender.sendMessage(ChatColor.WHITE + "[MiniGolf]" + ChatColor.RED + " You need to provide a course name. Like: \"/mgplay Rolling Greens <player_name>\".");
-                return true;
-            }
-            courseNameEndIndex = args.length - 1;
-            golfer = maybePlayer.get();
-        }
+        int courseNameEndIndex = getCourseNameEndIndex(args);
+        Player golfer = getGolfer(sender, args);
         String courseName = String.join(" ", Arrays.copyOfRange(args, 0, courseNameEndIndex));
         Optional<Course> maybeCourse = getPlugin().config().getCourse(courseName);
         if (maybeCourse.isEmpty()) {
@@ -58,6 +40,48 @@ public class PlayCommand implements CommandExecutor, TabCompleter
             Bukkit.getPluginManager().callEvent(new CoursePlayRequestedEvent(golfer, maybeCourse.get()));
         }
         return true;
+    }
+
+    private int getCourseNameEndIndex(String[] args) {
+        Optional<Player> maybePlayer = Bukkit.getOnlinePlayers().stream()
+            .filter(p -> p.getName().equals(args[args.length-1]))
+            .map(p -> (Player) p)
+            .findFirst();
+        if (maybePlayer.isPresent() || "@p".equals(args[args.length - 1])) {
+            return args.length - 1;
+        }
+        return args.length;
+    }
+
+    private Player getGolfer(CommandSender sender, String[] args) {
+        if (sender instanceof BlockCommandSender bcs) {
+            return findNearestPlayer(bcs);
+        }
+        Optional<Player> maybePlayer = Bukkit.getOnlinePlayers().stream()
+            .filter(p -> p.getName().equals(args[args.length-1]))
+            .map(p -> (Player) p)
+            .findFirst();
+        //Assume the player making the command is the player that will play the course
+        return maybePlayer.orElseGet(() -> (Player) sender);
+    }
+
+    private Player findNearestPlayer(BlockCommandSender bcs) {
+        Location senderLoc = bcs.getBlock().getLocation();
+        return Bukkit.getServer().getOnlinePlayers().stream()
+            .min((p1, p2) -> (int) (p1.getLocation().distanceSquared(senderLoc) - p2.getLocation().distanceSquared(senderLoc)))
+            .orElse(null);
+    }
+
+    private boolean hasIssues(CommandSender sender, String[] args) {
+        if (!sender.hasPermission("mgop")) {
+            sender.sendMessage(ChatColor.WHITE + "[MiniGolf]" + ChatColor.RED + " You do not have permission to run this command.");
+            return true;
+        }
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.WHITE + "[MiniGolf]" + ChatColor.RED + " You need to provide a course name. Like: \"/mgplay Rolling Greens\".");
+            return true;
+        }
+        return false;
     }
 
     @Nullable
